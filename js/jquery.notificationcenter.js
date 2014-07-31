50d13ab5b133adabb6641470eb940f083c7ea58b
Justin F. Hallett <thesin@gmail.com>
2014-07-31 13:36:00 -0600

    This was a mistake it causes more issues then it solves, just use offset when you intialize

--- js/jquery.notificationcenter.js
+++ js/jquery.notificationcenter.js
@@ -189,13 +189,6 @@
 						$('.notificationul').css({
 							top: nc.options.notification_offset
 						});
-
-						// Add top padding if panel isn't at the top
-						var bposition = $(nc.options.bodyElement).position();
-						if (bposition.top > 0)
-							$('.notificationul').css({
-								'padding-top': bposition.top
-							});
 					}
 
 					var index = inArray(type, nc.options.types);
